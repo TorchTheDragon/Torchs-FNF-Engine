3d50c269822b128eb21092afce37f5d72db38d7d
TorchTheDragon <1thatdragonboi@gmail.com>
2025-03-11 20:07:14 -0400

    You can now edit Ghost Notes through an event

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -88,7 +88,8 @@ class ChartingState extends MusicBeatState implements PsychUIEventHandler.PsychU
 		['Play Sound', "Value 1: Sound file name\nValue 2: Volume (Default: 1), ranges from 0 to 1"],
 		["Enemy Splashes", "Value 1: Enemy Note Splashes (true/false)\n\nValue 2: Enemy Strum Cover Splashes (true/false)"],
 		['Wobbly Notes', "Value 1: The wiggle factor 'x,y' Int Array\nValue 2: Who does it effect? Options are:\n'enemy', 'player', 'both', and 'none'\n'stop1' and 'stop2' also work\n'stop1' affects the enemy, 'stop2' affects the player\n\nSet value 1 to '0,0' or set 'none' in value 2 to remove\nwobble"],
-		['Cinematic Bars', "Value 1: The speed the bars move on screen \nand how long until they leave (ex. [2,4] [(speed),(wait)])\n\nValue 2: The distance the bars move"]
+		['Cinematic Bars', "Value 1: The speed the bars move on screen \nand how long until they leave (ex. [2,4] [(speed),(wait)])\n\nValue 2: The distance the bars move"],
+		['Edit Ghost Notes', 'Value 1: If the ghost should be colored, \nif the ghosts color should be affected by their custom note colors\nDefault Values: "true, true".\n\nValue 2: The tween time, the sliding distance.\nDefault Values: "0.4, 90.0"']
 	];
 	
 	public static var keysArray:Array<FlxKey> = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT]; //Used for Vortex Editor
--- source/torchsthings/objects/CustomEvents.hx
+++ source/torchsthings/objects/CustomEvents.hx
@@ -6,6 +6,7 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 import objects.Character;
+import torchsthings.objects.effects.GhostEffect;
 
 class CustomEvents {
     static var cinematicUpperY:Float = -350.0;
@@ -59,6 +60,28 @@ class CustomEvents {
                     FlxTween.tween(PlayState.instance.iconP1, {alpha: 0}, speed/2);
                     FlxTween.tween(PlayState.instance.iconP2, {alpha: 0}, speed/2);
                 }
+            case "Edit Ghost Notes":
+                // If the ghost is colored, if so, does it use per character colors or no?
+                var vals1:Array<String>;
+                if (value1 != null && value1 != '') {
+                    vals1 = value1.split(',');
+                    GhostEffect.coloredGhost = (vals1[0].trim().toLowerCase() == 'true');
+                    if (vals1.length >= 2) {
+                        GhostEffect.arrowColorGhost = (vals1[1].trim().toLowerCase() == 'true');
+                    }
+                }
+
+                // TweenTime, SlideDistance
+                // If slide distance is 0 then it uses scaling instead
+                var vals2:Array<String>;
+                if (value2 != null && value2 != '') {
+                    vals2 = value2.split(',');
+                    GhostEffect.tweenTime = Std.parseFloat(vals2[0].trim());
+                    if (vals2.length >= 2) {
+                        GhostEffect.slideDistance = Std.parseFloat(vals2[1].trim());
+                    }
+                }
+
         }
     }
 }
