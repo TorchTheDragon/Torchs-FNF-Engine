36982e6ea0133d997ec452881d248b2d90a6e9e8
TorchTheDragon <1thatdragonboi@gmail.com>
2025-05-20 22:08:28 -0400

    small change

--- source/options/VisualsSettingsSubState.hx
+++ source/options/VisualsSettingsSubState.hx
@@ -140,11 +140,13 @@ class VisualsSettingsSubState extends BaseOptionsMenu
 			["Stage", "Default", "Christmas", "ABot", "ABot-Pixel"]);
 		addOption(option);
 
+		#if desktop
 		var option:Option = new Option('Volume Bop',
 			"Should the speaker bop be dependant on user volume?\n\nTrue - Speakers bop the louder the overall volume is\nFalse - Speakers bop at song volume independent of user volume",
 			'volumeDependantBop',
 			'bool');
 		addOption(option);
+		#end
 
 		var option:Option = new Option('Show Credits',
 			'Uncheck this if you dont want to see the credits on song start',
